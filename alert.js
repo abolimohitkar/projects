9811069e26bb691b045f6a959d0c059e63c845f7
abolimohitkar <abolimohitkar11@gmail.com>
2023-02-17 16:40:47 +0530

    init

---
+++ alert.js
@@ -0,0 +1,7 @@
+"use strict";
+let user='aboli',age=30,marks=45;
+message='who i am?'
+alert(user + 'is my name' + ', my age is ' + age +'and marks are' + marks);
+alert('my name is aboli');
+alert('agrim is good');
+alert('hello')[1,2].forEach(alert);
---
+++ list.py
@@ -0,0 +1,5 @@
+p=('pen',30)
+p2=('pensil',45)
+l=[p,p2]
+for i in l:
+    print(l[i])
